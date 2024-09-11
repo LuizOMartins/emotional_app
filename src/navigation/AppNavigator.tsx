5fb819540b87001cf7737f70386c9448af25787b
LuizOMartins <luidigastech@gmail.com>
2024-09-11 19:31:20 -0300

    Add Lib

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 import HomePage from '../pages/HomePage';
@@ -8,18 +10,25 @@ import HomePage from '../pages/HomePage';
 export type RootStackParamList = {
     Login: undefined;
     Register: undefined;
-    Home: undefined
+    Home: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
+    const { isAuthenticated } = useContext(AuthContext);  // Pegar estado de autenticação
+
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={LoginPage} />
-                <Stack.Screen name="Register" component={RegisterPage} />
-                <Stack.Screen name="Home" component={HomePage} />
+            <Stack.Navigator initialRouteName={isAuthenticated ? "Home" : "Login"}>
+                {!isAuthenticated ? (
+                    <>
+                        <Stack.Screen name="Login" component={LoginPage} />
+                        <Stack.Screen name="Register" component={RegisterPage} />
+                    </>
+                ) : (
+                    <Stack.Screen name="Home" component={HomePage} />
+                )}
             </Stack.Navigator>
         </NavigationContainer>
     );
